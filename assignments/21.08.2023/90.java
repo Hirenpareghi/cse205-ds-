29ded19fd3167d774270395d79f5169586f76a82
Hirenpareghi <hiran.p@ahduni.edu.in>
2023-08-21 19:46:29 +0530

    90 in java

---
+++ assignments/21.08.2023/90.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // Sort the array to handle duplicates
+        backtrack(result, new ArrayList<>(), nums, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums, int start) {
+        result.add(new ArrayList<>(current));
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue; // Skip duplicates
+            }
+            current.add(nums[i]);
+            backtrack(result, current, nums, i + 1);
+            current.remove(current.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 2};
+        List<List<Integer>> subsets = solution.subsetsWithDup(nums);
+        for (List<Integer> subset : subsets) {
+            System.out.println(subset);
+        }
+    }
+}
