807242bc7e1dc71628e0e9fa00b362ef16610759
Hirenpareghi <hiran.p@ahduni.edu.in>
2023-09-03 12:42:52 +0530

    best time

---
+++ assignments/28.08.2023/121.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            
+            int potentialProfit = price - minPrice;
+            
+   
+            maxProfit = Math.max(maxProfit, potentialProfit);
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        Solution solution = new Solution();
+        int maxProfit = solution.maxProfit(prices);
+        System.out.println("Maximum Profit: " + maxProfit);
+    }
+}
