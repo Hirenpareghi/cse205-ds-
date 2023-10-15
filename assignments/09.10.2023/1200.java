9fea99146525595bdea7d40f79619e0635dc8e68
Hirenpareghi <hiran.p@ahduni.edu.in>
2023-10-15 18:35:05 +0530

    Minimum Absolute Difference

---
+++ assignments/09.10.2023/1200.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+     //      TIME COMPLEXITY IS o(nlogn)
+ //     SPACE COMPLEXITY IS o(n);   
+        
+        Arrays.sort(arr);
+        List<List<Integer>> ans = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+        
+        for(int i=0; i<arr.length-1; i++){
+         if(arr[i+1] - arr[i] < min)   {
+             min = arr[i+1] - arr[i];
+         }
+        }
+        
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i+1] - arr[i] == min){
+                
+  // ans.add(Arrays.asList(arr[i],arr[i+1])); 
+// we can add the elements by above commented method too
+                
+                List <Integer> pair = new ArrayList<>(2);
+                pair.add(arr[i]);
+                pair.add(arr[i+1]);
+                ans.add(pair);
+                
+            }
+        }
+        return ans;
+		   
+    }
+}
